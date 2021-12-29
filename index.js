cbb263e9d539f8a9bf6552587ac74f77c0c0cba8
Hikmet Meskic <hikmet.meskic2018@size.ba>
2021-12-29 16:31:46 +0100

    Add colors module, color red error, else color green

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ const myArg = process.argv[2];
 // Importing modules 'franc' and 'langs'
 import { franc } from 'franc';
 import langs from 'langs';
+import colors from 'colors';
 
 // Passing argument to franc function to get ISO guessed language
 const ISOlanguage = franc(myArg, { minLength: 5 });
-
+console.log(franc("Nisi dobro to uradio"));
 if (ISOlanguage === 'und') {
     // Display error when it couldn't find appropriate language
-    console.error('Sorry, could not figure it out! Try with more sample text.')
+    console.error('Sorry, could not figure it out! Try with more sample text.'.red)
 }
 else {
     // Get full name of language and 'console.log' it
     const guessedLanguage = langs.where("3", ISOlanguage);
-    console.log(`Guessed language: ${guessedLanguage.name}`);
+    console.log(`Guessed language: ${guessedLanguage.name}`.green);
 }
